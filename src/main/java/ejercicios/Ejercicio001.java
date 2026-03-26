7c303f639b0c4f476961adcfedf68af55bdc9d1c
Emiliano Leonel Toro Mora <emitormor.alu.edu.gva.es>
2026-03-26 19:41:33 +0100

    Ejercicio001 completado

--- src/main/java/ejercicios/Ejercicio001.java
+++ src/main/java/ejercicios/Ejercicio001.java
@@ -6,24 +6,34 @@ import java.util.Scanner;
 public class Ejercicio001 {
 	//COMPLETAR METODO
 	public double suma(double d, double e) {
-        return 0.0;
+        return d + e;
     }
 	//COMPLETAR METODO
     public double resta(double d, double e) {
-        return 0.0;
+        return d - e;
     }
     //COMPLETAR METODO
     public double multiplicacion(double d, double e) {
-        return 0.0;
+        return d * e;
     }
     //COMPLETAR METODO
     public double division(double d, double e) {
-        return 0.0;
+    	if (e == 0) {
+    		throw new ArithmeticException("No se puede dividir entre cero");
+    	}
+        return d / e;
     }
 
     //COMPLETAR METODO
     public void mostrarTodasOperaciones(double num1, double num2) {
-        
+        System.out.println("Suma: 	" + suma(num1, num2));
+        System.out.println("Resta: 	" + resta(num1, num2));
+        System.out.println("Multiplicación: 	" + multiplicacion(num1, num2));
+        if (num2 != 0) {
+        	System.out.println("División: 	" + division(num1, num2));
+        } else {
+        	System.out.println("División: 	 No se puede dividir entre cero");
+        }
     }
 
     public static void main(String[] args) {
